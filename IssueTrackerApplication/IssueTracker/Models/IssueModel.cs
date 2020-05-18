9a2c455c7cfa078dc22dcbcb5efbe01db98cb8fa
RileyWium <rjwium@gmail.com>
2020-05-17 22:02:58 -0700

    Partial refinement of data models

--- IssueTrackerApplication/IssueTracker/Models/IssueModel.cs
+++ IssueTrackerApplication/IssueTracker/Models/IssueModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IssueTracker.Models
 {
@@ -17,11 +18,15 @@ namespace IssueTracker.Models
     }
     public class IssueModel
     {
+        [Key]
         [DisplayAttribute(Name = "Issue ID")]
+        //auto
         public int ID { get; set; }
 
         [DisplayAttribute(Name = "Project ID")]
+        [ForeignKey("Project")]
         //[Range(100000, 999999, ErrorMessage = "Need valid Project ID of six numbers.")]
+        //auto
         public int ProjID { get; set; }
 
         [Display(Name ="Issue Name")]
@@ -29,19 +34,21 @@ namespace IssueTracker.Models
         [Required(ErrorMessage ="Issue Name required.")]
         public string IssName { get; set; }
 
-        [Display(Name ="Creation Date")]
+        [Display(Name ="Report Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
+        //auto
         public DateTime CreationDate { get; set; }
 
         [Display(Name = "Issue Description")]
         [StringLength(350, ErrorMessage = "The {0} cannot exceed {1} characters. ")]
         public string IssDescription { get; set; }
+        public UserModel IssueAssignee { get; set; }
+        public UserModel IssueReporter { get; set; }
 
-        //public string IssueAssignee { get; set; } put this in later
-        //public string IssueReporter { get; set; } auto set
         //question mark after Status(?) means its nullable
-        public Status? IssStatus { get; set; }//probably shouldn't be nullable
-        public Priority? IssPriority { get; set; }
-
+        public Status IssStatus { get; set; }
+        public Priority IssPriority { get; set; }
         public virtual ProjectModel Project { get; set; }
     }
 }
--- IssueTrackerApplication/IssueTracker/Models/ProjectModel.cs
+++ IssueTrackerApplication/IssueTracker/Models/ProjectModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IssueTracker.Models
 {
@@ -11,14 +12,19 @@ namespace IssueTracker.Models
         
         //[DisplayAttribute(Name = "Project ID")]
         //[Range(100000,999999, ErrorMessage = "Need valid Project ID of six numbers.")]
+        [Key]
+        //auto
         public int ID { get; set; }
-        public int CreatorID { get; set; }
+        [ForeignKey("Master User ID")]
+        //auto
+        public int MasterUserID { get; set; }
 
         //[Display(Name ="Project Name")]
         //[StringLength(50, ErrorMessage = "The {0} cannot exceed {1} characters. ", MinimumLength =1)]
         //[Required(ErrorMessage ="You need a name for the Project.")]
         public string ProjName { get; set; }        
-        public virtual UserModel CreatorUser { get; set; }
+        //auto
+        public virtual UserModel MasterUser { get; set; }
         public virtual ICollection<UserModel> Users { get; set; }
         public virtual ICollection<IssueModel> Issues { get; set; }
     }
